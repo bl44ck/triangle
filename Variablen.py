646715deecee9250c92e890bbbb8bfbc8aabd06f
Simon <78374406+bl44ck@users.noreply.github.com>
2021-02-06 10:48:26 +0100

    Delete Variablen.py

--- Variablen.py
+++
@@ -1,6 +0,0 @@
-# Variablen
-    gueltig = False
-    winkel_c = 0
-    winkel = ('rechtwinkliges', 'spitzwinkliges', 'stumpfwinkliges')
-    seiten = ('gleichseitiges', 'ungleichseitiges', 'gleichschenkliges')
-    ausgabe = []
