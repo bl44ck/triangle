93435abfce0785f0e48dbef5d313b4427bd92c35
Erion93 <78639406+Erion93@users.noreply.github.com>
2021-02-06 09:46:50 +0100

    Add files via upload

---
+++ dreicek_eingabe.py
@@ -0,0 +1,15 @@
+
+
+a = int(input ("Bitte seite a eingeben:"))
+b = int(input ("Bitte seite b eingeben:"))
+c = int(input ("Bitte seite c eingeben:"))
+
+
+
+if (a + b <= c) or (b + c <= a) or (c + a <= b):
+    print("Eingabe nich möglich")
+
+else:
+    print("Eingabe ok")
+
+
