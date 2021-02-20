c2bfccf100985538b2e6612e2a8d996a6c11c7b1
oli-tech <78639592+oli-tech@users.noreply.github.com>
2021-02-20 17:07:54 +0100

    Update triangleType.py

--- triangleType.py
+++ triangleType.py
@@ -1,2 +1,9 @@
+
+
+#a = int(input ("Bitte seite a eingeben:"))
+#b = int(input ("Bitte seite b eingeben:"))
+#c = int(input ("Bitte seite c eingeben:"))
+
+
 def triangleType(a,b,c):
-    "iso"
+    
