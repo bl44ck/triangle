4b236eb198b5926d458acd8f369b57bcd7433fee
oli-tech <78639592+oli-tech@users.noreply.github.com>
2021-02-20 17:37:24 +0100

    Update triangleType.py

--- triangleType.py
+++ triangleType.py
@@ -1,9 +1,8 @@
-#Triangletyp-definition by Oliver
+#Triangletype-definition by Oliver
 
 #a = int(input ("Bitte Seite a eingeben:"))
 #b = int(input ("Bitte Seite b eingeben:"))
 #c = int(input ("Bitte Seite c eingeben:"))
-
 
 def triangleType(a,b,c):
     
@@ -12,4 +11,4 @@ if (a=b=c)
 elif (a=b) or (a=c) or (b=c)
     return ("iso")
 else
-    return ("unequ"
+    return ("unequ")
