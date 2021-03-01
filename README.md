f311a776bab4a4eb4faf9bd8766babdc7e0baec6
Simon <request@actofaces.com>
2021-03-01 14:19:11 +0100

    Updated code, various bugfixes and code optimization

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Welcome to our Project triangle, where you can input three float values for the 
 - Angle *(acute, obtuse or rectangular triangle)*
 - Type of Triangle *(equilateral, right or isosceles triangle)*
 
+Note: Only works with Integers!
+
 Please see attached LICENSE file for license details.
--- triangle.py
+++ triangle.py
@@ -3,35 +3,35 @@ from angle import angle
 from isTriangle import isTriangle
 from triangleType import triangleType
 
-print("Welcome to our Triangle calculator, please enter sides a, b and c, 0 (Zero)" +
-    " will exit the program.")
+def output():
+    """Outputs the variables in one string.
+
+    Args:
+        no args
+
+    Returns:
+        no return, prints the text as seen below.
+    """
+    angle_output = angle(a,b,c)
+    type_output = triangleType(a,b,c)
+    if isTriangle(a,b,c) == True:
+        print(f"This is a valid, {angle_output} and {type_output} triangle!")
+    else:
+        print("This is no valid triangle, please check your inputs!")
 
 while True:
 
+    print("Please enter sides a, b and c, 0 (Zero)" +
+    " will exit the program. Only Integers are allowed.")
+
+#input
     a = int(input("Enter side a: "))
     if a == 0:
         break
     b = int(input("Enter side b: "))
     c = int(input("Enter side c: "))
 
-
-    if isTriangle(a,b,c) == True:
-        print("This is a valid triangle,")
-
-        if angle(a,b,c) == 90:
-            print("its rectangular")
-        elif angle(a,b,c) > 90:
-            print("its obtuse")
-        else:
-            print("its acute")
-
-        if triangleType(a,b,c) == 0 :
-            print("and equilateral.")
-        elif triangleType(a,b,c) == 2:
-            print("and unequal sided.")
-        elif triangleType(a,b,c) == 1:
-            print("and isosceled.")
-
-    else:
-        print("This is no possible combination for a Triangle, try again.")
-        continue
+#output
+    output()
+    
+    continue
